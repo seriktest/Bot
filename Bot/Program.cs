73df7e633d52d71985719e0500f5b0a0046ac93f
Serik <podelu000@gmail.com>
2022-11-21 19:12:45 +0300

    init

---
+++ Bot/Program.cs
@@ -0,0 +1,18 @@
+﻿using Bot;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+
+var botClient = new TelegramBotClient("5479616432:AAFiMvKbY480ClvWUVOAnoNxmSQlXdpJ0KM");
+
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, _) => cts.Cancel(); // Чтобы отловить нажатие ctrl+C 
+
+var handler = new UpdateHandler();
+var receiverOptions = new ReceiverOptions();
+
+botClient.StartReceiving(handler, receiverOptions, cancellationToken: cts.Token);
+
+Console.WriteLine("Bot started. Press ^C to stop");
+await Task.Delay(-1, cancellationToken: cts.Token);
+Console.WriteLine("Bot stopped");
+Console.WriteLine("Hello, World!");
---
+++ Bot/UpdateHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Bot;
+
+class UpdateHandler : IUpdateHandler
+{
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        Debug.WriteLine(JsonSerializer.Serialize(update));
+        // Вообще, для обработки сообщений лучше подходит паттерн "Цепочка обязанностей", но для примера тут switch-case
+        // https://refactoring.guru/ru/design-patterns/chain-of-responsibility
+        switch (update)
+        {
+            case
+            {
+                Type: UpdateType.Message,
+                Message: { Text: { } text, Chat: { } chat },
+            } when text.Equals("/start", StringComparison.OrdinalIgnoreCase):
+            {
+                await botClient.SendTextMessageAsync(chat!, "Добро пожаловать на борт, добрый путник!", cancellationToken: cancellationToken);
+                break;
+            }
+            case
+            {
+                Type: UpdateType.Message,
+                Message.Chat: { } chat
+            }:
+            {
+                await botClient.SendTextMessageAsync(chat!, "Привет-привет!!", cancellationToken: cancellationToken);
+                break;
+            }
+        }
+    }
+    public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        Console.Error.WriteLine(exception);
+        return Task.CompletedTask;
+    }
+}
+
